d135fb571fa8ce5f8bd4cc2a8493a796303ee191
Andrew Harding <azdagron@gmail.com>
2020-01-03 16:37:11 -0700

    messing with the build
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- .travis/run-unit-tests.sh
+++ .travis/run-unit-tests.sh
@@ -6,12 +6,15 @@ REPODIR=$(git rev-parse --show-toplevel)
 
 COVERPROFILE=
 if [ -n "${COVERALLS_TOKEN}" ]; then
+    echo "Installing goveralls..."
     COVERPROFILE=profile.cov
     go get github.com/mattn/goveralls@v0.0.4
 fi
 
+echo "Running unit tests..."
 make -C "${REPODIR}" COVERPROFILE="${COVERPROFILE}" test
 
 if [ -n "${COVERALLS_TOKEN}" ]; then
+    echo "Pushing code coverage..."
     "$(go env GOPATH)"/bin/goveralls -service=travis-ci
 fi
